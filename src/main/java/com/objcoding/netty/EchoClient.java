4c4b07ccc2e6be4ce6ef250e1502dd6507a5799b
zch <zhangchenghui.dev@gmail.com>
2018-11-09 11:13:31 +0800

    update

--- src/main/java/com/objcoding/netty/EchoClient.java
+++ src/main/java/com/objcoding/netty/EchoClient.java
@@ -1,14 +1,11 @@
 package com.objcoding.netty;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-
-import java.net.InetSocketAddress;
 
 /**
  * @author zhangchenghui.dev@gmail.com
@@ -16,47 +13,32 @@ import java.net.InetSocketAddress;
  */
 public class EchoClient {
 
-    private final String host;
-    private final int port;
-
-    public EchoClient(String host, int port) {
-        this.host = host;
-        this.port = port;
+    public static void main(String[] args) {
+        start();
     }
 
-    public void start() throws Exception {
+    private static void start() {
         EventLoopGroup group = new NioEventLoopGroup();
-
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group)
-                    .channel(NioSocketChannel.class)
-                    .remoteAddress(new InetSocketAddress(host, port))
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new EchoClientHandler());
-                        }
-                    });
-            // 连接到远程节点，阻塞等待直到连接完成
-            ChannelFuture f = b.connect().sync();
-            // 阻塞，直到 Channel 关闭
-            f.channel().closeFuture().sync();
-        } finally {
-            // 关闭线程池并且释放所有的资源
-            group.shutdownGracefully().sync();
-        }
+        new Bootstrap()
+                // 1. 定义线程模型
+                .group(group)
+                // 2. 定义IO模型
+                .channel(NioSocketChannel.class)
+                // 3. IO 处理逻辑
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new EchoClientHandler());
+                    }
+                })
+                .connect("127.0.0.1", 8081)
+                .addListener(future -> {
+                    if (future.isSuccess()) {
+                        System.out.println("连接成功!");
+                    } else {
+                        System.err.println("连接失败!");
+                    }
+                });
     }
-
-    public static void main(String[] args) throws Exception {
-//        if (args.length != 2) {
-//            System.err.println("Usage: " + EchoClient.class.getSimpleName() + " <host> <port>");
-//            return;
-//        }
-//        String host = args[0];
-//        int port = Integer.parseInt(args[1]);
-        new EchoClient("127.0.0.1", 8081).start();
-    }
-
 
 }
--- src/main/java/com/objcoding/netty/EchoServer.java
+++ src/main/java/com/objcoding/netty/EchoServer.java
@@ -1,60 +1,45 @@
 package com.objcoding.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-import java.net.InetSocketAddress;
-
 /**
  * @author zhangchenghui.dev@gmail.com
  * @since 2018/11/2
  */
 public class EchoServer {
-    private final int port;
 
-    public EchoServer(int port) {
-        this.port = port;
+    public static void main(String[] args) {
+        start();
     }
 
-    public static void main(String[] args) throws Exception {
-//        if (args.length != 1) {
-//            System.err.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
-//        }
-//        int port = Integer.parseInt(args[0]);
-        new EchoServer(8081).start();
-    }
-
-    public void start() throws Exception {
+    private static void start() {
         final EchoServerHandler serverHandler = new EchoServerHandler();
-        // 创建 EventLoopGroup
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            // 创建 ServerBootStrap
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(group)
-                    .channel(NioServerSocketChannel.class)
-                    .localAddress(new InetSocketAddress(port))
-                    // 添加 处理器
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(serverHandler);
-                        }
-                    });
+        // 1.创建线程模型
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
 
-            // 异步地绑定服务器，调用sync()方法阻塞等待知道绑定完成
-            ChannelFuture f = b.bind().sync();
-            // 获取 Channel 的 CloseFuture，并 且阻塞当前线 程直到它完成
-            f.channel().closeFuture().sync();
-
-        } finally {
-            //  关闭 EventLoopGroup， 释放所有的资源
-            group.shutdownGracefully().sync();
-        }
+        new ServerBootstrap().group(bossGroup, workerGroup)
+                // 2. 定义IO模型
+                .channel(NioServerSocketChannel.class)
+                // 3. 定义读写处理逻辑
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(serverHandler);
+                    }
+                })
+                .bind(8081)
+                .addListener((future) -> {
+                    if (future.isSuccess()) {
+                        System.out.println("端口绑定成功!");
+                    } else {
+                        System.err.println("端口绑定失败!");
+                    }
+                });
     }
 }
